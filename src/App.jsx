006ea595b5d08c86078d9db9ec68dffb2e1a9e3a
dolev_dublon <d.dublon@alphaomega-eng.com>
2023-04-25 14:40:51 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import MyResponsiveHeatMap from "./MyResponsiveHeatMap";
+
+const App = () => {
+  const data = [
+//  ????
+  ];
+
+  return (
+    <>
+      <div>App</div>
+      <MyResponsiveHeatMap data={data} />
+    </>
+  );
+};
+
+export default App;
